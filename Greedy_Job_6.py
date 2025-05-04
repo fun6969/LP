2d7e9e37a956cf3d1a47b8200de5f818b00bb09b
fun6969 <dparth454@gmail.com>
2025-05-05 01:30:44 +0530

    Add files via upload

--- Greedy_Job_6.py
+++ Greedy_Job_6.py
@@ -1,22 +1,28 @@
-def dfs(graph,start,visited=None):
-    if visited is None:
-        visited=set()
+def job_schedule(jobs):
+    jobs.sort(key=lambda x:x[2], reverse=True)
+    max_deadline=max(job[1] for job in jobs)
+    slot=[False]*max_deadline
+    schedule=[None]*max_deadline
+    total_profit=0
 
-    visited.add(start)
-    print(start,end=" ")
+    for job in jobs:
+        job_id,job_deadline,job_profit=job
+        for i in range(min(job_deadline,max_deadline) -1,-1,-1):
+            if not slot[i]:
+                slot[i]=True
+                schedule[i]=job_id
+                total_profit+=job_profit
+                break
+    print("schedule job: ", [job for job in schedule if job])
+    print("Total profit:" ,total_profit)
 
+jobs = [
+    ('J1', 2, 60),
+    ('J2', 1, 10),
+    ('J3', 3, 20),
+    ('J4', 2, 40),
+    ('J5', 1, 20)
+]
 
-    for neighbour in graph[start]:
-        if neighbour not in visited:
-            dfs(graph,neighbour,visited)
+job_schedule(jobs)
 
-graph={
-    1:[3,4,2],
-    2:[5,6],
-    3:[4],
-    4:[5],
-    5:[6],
-    6:[]
-}
-
-dfs(graph,1)
